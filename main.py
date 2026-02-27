bcf7075cf6640cea7c8fa81838867d22b4f81ec2
baofuzhang <baofuzhang0402@163.com>
2026-02-27 16:04:50 +0800

    Auto commit at 2026-02-27 16:04:50

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ ENDTIME = "14:01:00"  # 根据学校的预约座位时间+1min即可
 
 ENABLE_SLIDER = False  # 是否有滑块验证（调试阶段先关闭）
 MAX_ATTEMPT = 30  # 最大尝试次数（减少到30次，确保3个配置都能尝试）
-RESERVE_NEXT_DAY = True  # 预约明天而不是今天的
+RESERVE_NEXT_DAY = False  # 预约明天而不是今天的
 
 
 # 是否在每一轮主循环中都重新登录。
